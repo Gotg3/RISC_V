36512e4ec086e0c787bfa6fdf6206179cbd74895
laura19101996 <73799197+laura19101996@users.noreply.github.com>
2020-12-07 17:32:57 +0100

    risc_package and imm_gen vhdl
    new vhdl of immediate generator and update of risc_package vhdl

--- VHDL/src/risc_package.vhd
+++ VHDL/src/risc_package.vhd
@@ -4,9 +4,12 @@ use ieee.numeric_std.all;
 
 package risc_package is
 
-	constant instruction_parallelism: integer:=32;    -- instruction parallelism
-	constant number_regs: integer:=32;
-	constant data_parallelism: integer := 32; --data parallelism
-	constant length_in_RF: integer :=5; -- number of bits for the register file inputs
+    constant sel_imm					: integer:=3; 		-- immediate selector
+	constant instruction_parallelism	: integer:=32;    	-- instruction parallelism
+ 	constant number_regs				: integer:=32;		-- number of regs in RF
+	constant data_parallelism			: integer := 32;	-- data parallelism
+	constant length_in_RF				: integer :=5; 		-- number of bits for the register file inputs
+	constant alu_ctrl					: integer :=4; 		-- control bits for ALU
+	constant srx						: integer :=5;      -- amount of right shift
 	
 end package risc_package;
