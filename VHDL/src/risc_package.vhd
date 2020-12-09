97d5977931009aba0ab406c30bbc16706b75a94b
laura19101996 <73799197+laura19101996@users.noreply.github.com>
2020-12-09 10:50:12 +0100

    update package
    a constant added to the package

--- VHDL/src/risc_package.vhd
+++ VHDL/src/risc_package.vhd
@@ -4,12 +4,13 @@ use ieee.numeric_std.all;
 
 package risc_package is
 
-	constant trailing_zeros            : integer:=12;  -- trailing zeros for filling operation
-   constant sel_imm				   : integer :=3; 	-- immediate selector
+    constant mux_ctrl 				   : integer :=2;   --mux control bit
+	constant trailing_zeros            : integer :=12;  -- trailing zeros for filling operation
+    constant sel_imm				   : integer :=3; 	-- immediate selector
 	constant instruction_parallelism   : integer :=32;  -- instruction parallelism
  	constant number_regs			   : integer :=32;  -- number of regs in RF
 	constant data_parallelism		   : integer :=32;	-- data parallelism
-	constant length_in_RF			   : integer :=5;   -- number of bits for the register file inputs
+	constant length_in_RF			   : integer :=5;   -- number of bits of the address at the output of the register file
 	constant alu_ctrl				   : integer :=4; 	-- control bits for ALU
 	constant srx					   : integer :=5;   -- amount of right shift
 	constant funct					   : integer :=3;  	--function bits			
