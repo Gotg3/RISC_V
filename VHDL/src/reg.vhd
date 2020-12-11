e8d1ccaf6ac631dd97a471655e24c26cfe65d2a3
laura19101996 <73799197+laura19101996@users.noreply.github.com>
2020-12-11 11:22:02 +0100

    reg

---
+++ VHDL/src/reg.vhd
@@ -0,0 +1,26 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+library work;
+use work.risc_package.all;
+
+entity reg is
+port (
+    clk: in std_logic;
+	rst: in std_logic;
+    r :  in std_logic_vector(n-1 downto 0);
+	q :  out std_logic_vector(n-1 downto 0));
+end reg;
+
+architecture  behavioural of reg is
+begin
+	process (clk, rst)
+		begin
+		if (rst = '1') then
+				q <= (others => '0');
+			elsif (clk'event and clk = '1') then
+				q <= r;
+		    end if;
+	end process;
+end behavioural;
