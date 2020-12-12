17e2c61933419cb0d5840d5059cb8df0afd8839b
laura19101996 <73799197+laura19101996@users.noreply.github.com>
2020-12-12 12:41:00 +0100

    CU

--- VHDL/src/CU.vhd
+++ VHDL/src/CU.vhd
@@ -8,7 +8,7 @@ use work.risc_package.all;
 entity CU is 
 	port(
 		 rst: in std_logic; --attivo alto
-	     opcode: in std_logic_vector(opcode_size -1 downto 0);
+	    opcode: in std_logic_vector(opcode_size -1 downto 0);
 		 funct3: in std_logic_vector(funct -1 downto 0);
 		 ctrl: out std_logic_vector(out_ctrl -1 downto 0) -- 3 bits immediate; 3 bits ((2 bits)MemtoReg, (1 bit) RegWrite) wb; 3 bits (MemWrite, MemRead, BranchCtrl) mem; 5 bits (ALU_op (3 bits), ALU_src1(1 bit), ALU_src2(1 bit))ex
 	);
@@ -16,16 +16,18 @@ end CU;
 
 architecture arch of CU is 
 
+begin
+
 process(rst, funct3, opcode)
 begin
-	if(rst='1')
-       ctrl<=(others'0');
+	if(rst='1') then
+       ctrl<=(others=>'0');
 	  
 	else
 	
 	case opcode is
 	
-	when => "0110011"   --ADD, XOR, SLT 
+	when "0110011" =>   --ADD, XOR, SLT 
 	
 		if (funct3="000") then --ADD
 		
@@ -37,7 +39,7 @@ begin
 		
 		elsif (funct3="010") then --SLT
 		
-			crtl<="111011100010000";
+			ctrl<="111011100010000";
 		
 		else
 		
@@ -45,7 +47,7 @@ begin
 		
 		end if;
 	
-	when => "0010011" --ADDI, SRAI, ANDI
+	when "0010011" =>--ADDI, SRAI, ANDI
 	
 		if (funct3="000") then --ADDI
 		
@@ -57,7 +59,7 @@ begin
 		
 		elsif (funct3="111") then --ANDI
 		
-			crtl<="000011100000101";
+			ctrl<="000011100000101";
 		
 		else
 		
@@ -65,29 +67,35 @@ begin
 		
 		end if; 
 		
-	when => "0010111" --AUIPC
+	when "0010111" => --AUIPC
 	
-		crtl<="001011100001011";
+		ctrl<="001011100001011";
 		
-	when => "0110111" --LUI
+	when "0110111" =>--LUI
 	
-		crtl<="001010100011100";
+		ctrl<="001010100011100";
 		
-	when => "1100011" --BEQ
+	when "1100011" => --BEQ
 	
-		crtl<="100100000110100";
+		ctrl<="100100000110100";
 	
-	when => "0000011" --LW
+	when "0000011" => --LW
 	
-		crtl<="000000101000001";
+		ctrl<="000000101000001";
 		
-	when => "1101111" --JAL
+	when "1101111" => --JAL
 	
-		crtl<="010001100011100";
+		ctrl<="010001100011100";
 
-	when => "0100011" --SW
+	when "0100011" => --SW
 	
 		ctrl<="011100010001101";
 		
+	when others=>
+	
+		ctrl<=(others=>'0');
+		
 	end case;
+end if;
+end process;
 end arch;
