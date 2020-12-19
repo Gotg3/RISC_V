c483c2eb72d69722de9f74032194c0acddf7da6c
Gianluca <gianlucagoti1996@gmail.com>
2020-12-19 22:44:56 +0100

    update tb, upload script for simulation

--- VHDL/tb/tb_RISCV.vhd
+++ VHDL/tb/tb_RISCV.vhd
@@ -15,20 +15,20 @@ component RISCV is
 	--in
 	clk					: in std_logic;
 	rst					: in std_logic;
-	instruction_ID_in	: in std_logic_vector(instruction_parallelims-1 downto 0);
+	instruction_ID_in	: in std_logic_vector(instruction_parallelism-1 downto 0);
 	Read_data_WB_in		: in std_logic_vector(data_parallelism-1 downto 0);
 	--out
-	mux_IF_out			: out std_logic_vector(instruction_parallelims-1 downto 0);
-	PC_Write_ID_out		: out std_logic_vector(instruction_parallelims-1 downto 0);
+	mux_IF_out			: out std_logic_vector(instruction_parallelism-1 downto 0);
+	PCWrite_ID_out		: out std_logic;
 	IF_ID_Write_ID_out	: out std_logic;
 	ALUout_EX_out		: out std_logic_vector(data_parallelism-1 downto 0);
 	ALU_bypass_EX_out	: out std_logic_vector(data_parallelism-1 downto 0);
 	MemWrite			: out std_logic;
-	MemRead				: out std_logic;	
+	MemRead				: out std_logic	
 	);
 end component;
 
-component DM is
+component data_memory is
 port (
 			ADDR: 	 		in std_logic_vector(address_parallelism-1 downto 0);
 			DATA_IN:  		in std_logic_vector(data_parallelism-1 downto 0);
@@ -40,7 +40,7 @@ port (
 			);
 end component;
 
-component IM is
+component instruction_memory is
 	port (
 			ADDR: 	 		 in std_logic_vector(address_parallelism-1 downto 0);
 			DATA_IN: 		 in std_logic_vector(instruction_parallelism-1 downto 0);
@@ -58,16 +58,16 @@ end component;
 --in
 signal clk						:std_logic:='0';
 signal rst						:std_logic:='0';
-signal instruction_ID_in_s		:std_logic_vector(instruction_parallelims-1 downto 0);
+signal instruction_ID_in_s		:std_logic_vector(instruction_parallelism-1 downto 0);
 signal Read_data_WB_in_s		:std_logic_vector(data_parallelism-1 downto 0);
 --out
-signal mux_IF_out_s				:std_logic_vector(instruction_parallelims-1 downto 0);
+signal mux_IF_out_s				:std_logic_vector(instruction_parallelism-1 downto 0);
 signal IF_ID_Write_ID_out_s		:std_logic;
 signal ALUout_EX_out_s			:std_logic_vector(data_parallelism-1 downto 0);
 signal ALU_bypass_EX_out_s		:std_logic_vector(data_parallelism-1 downto 0);
-signal PC_Write_ID_out_s		:std_logic_vector(instruction_parallelims-1 downto 0);
-signal MemWrite_s				:std_logic;
-signal MemRead_s	            :std_logic;
+signal PC_Write_ID_out_s		:std_logic;
+signal MemWrite_s			:std_logic;
+signal MemRead_s	           	 :std_logic;
 
 --IM signals
 
@@ -90,14 +90,14 @@ signal RAM_WR_s:			std_logic:='0'; --read always
 	IF_ID_Write_ID_out	=> IF_ID_Write_ID_out_s,
 	ALUout_EX_out		=> ALUout_EX_out_s,
 	ALU_bypass_EX_out	=> ALU_bypass_EX_out_s,
-	PC_Write_ID_out		=> PC_Write_ID_out_s,
-	MemWrite			=> MemWrite_s,
-	MemRead	            => MemRead_s	         
+	PCWrite_ID_out		=> PC_Write_ID_out_s,
+	MemWrite		=> MemWrite_s,
+	MemRead	           	 => MemRead_s	         
 	);
 	
 	
 	--IM declaration
-	IM_comp : IM
+	IM_comp : instruction_memory
 	port map(
 	ADDR			=> mux_IF_out_s, 	
 	DATA_IN			=> DATA_IN_s,
@@ -111,7 +111,7 @@ signal RAM_WR_s:			std_logic:='0'; --read always
 	
 	
 	--DM declaration
-	DM_comp	: DM
+	DM_comp	: data_memory
 	port map(
 	ADDR 	 	=> ALUout_EX_out_s,
 	DATA_IN  	=> ALU_bypass_EX_out_s,
@@ -127,9 +127,11 @@ signal RAM_WR_s:			std_logic:='0'; --read always
 	clk <= not(clk) after 20 ns;--periodo 40 ns
 	
 	stimuli: process
-	
+	begin 
+
 	rst<='1';
 	wait for 40 ns;
+	rst<='0';
 	wait;
 	
 	end process stimuli;
