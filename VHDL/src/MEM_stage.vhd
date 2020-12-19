4d819b88698701c8f22adb3b25c540429ceac112
laura19101996 <73799197+laura19101996@users.noreply.github.com>
2020-12-19 12:35:11 +0100

    update

--- VHDL/src/MEM_stage.vhd
+++ VHDL/src/MEM_stage.vhd
@@ -9,23 +9,23 @@ entity MEM_stage is
 
     port(
 		--inputs
-		WB_MEM_in:  		in std_logic_vector(WB_length -1 downto 0);
-		BranchCtrl: 		in std_logic;
-		JAL_PC_4_MEM_in:    in std_logic_vector(instruction_parallelism - 1 downto 0);
-		rd_MEM_in:          in std_logic_vector(dest_reg-1 downto 0);
-		TAddr_MEM_in:		in std_logic_vector(instruction_parallelism-1 downto 0);
-		z_MEM_in:			in std_logic;
-		immediate_MEM_in:   in std_logic_vector(data_parallelism - 1 downto 0);
-		ALUout_MEM_in:      in std_logic_vector(data_parallelism-1 downto 0);
+		WB_MEM_in         :  in std_logic_vector(WB_length -1 downto 0);
+		BranchCtrl		  :  in std_logic;
+		JAL_PC_4_MEM_in	  :  in std_logic_vector(instruction_parallelism - 1 downto 0);
+		rd_MEM_in         :  in std_logic_vector(dest_reg-1 downto 0);
+		TAddr_MEM_in      :	 in std_logic_vector(instruction_parallelism-1 downto 0);
+		z_MEM_in          :	 in std_logic;
+		immediate_MEM_in  :  in std_logic_vector(data_parallelism - 1 downto 0);
+		ALUout_MEM_in     :  in std_logic_vector(data_parallelism-1 downto 0);
 		
 		--outputs
-		WB_MEM_out:			out std_logic_vector(WB_length -1 downto 0);
-		JAL_PC_4_MEM_out:   out std_logic_vector(instruction_parallelism - 1 downto 0);
-		ALUout_MEM_out:     out std_logic_vector(data_parallelism-1 downto 0);
-		immediate_MEM_out:  out std_logic_vector(data_parallelism - 1 downto 0);
-		rd_MEM_out:         out std_logic_vector(dest_reg-1 downto 0);
-		PCsrc:              out std_logic;
-		TAddr_MEM_out:		out std_logic_vector(instruction_parallelism-1 downto 0)
+		WB_MEM_out        :	 out std_logic_vector(WB_length -1 downto 0);
+		JAL_PC_4_MEM_out  :  out std_logic_vector(instruction_parallelism - 1 downto 0);
+		ALUout_MEM_out    :  out std_logic_vector(data_parallelism-1 downto 0);
+		immediate_MEM_out :  out std_logic_vector(data_parallelism - 1 downto 0);
+		rd_MEM_out        :  out std_logic_vector(dest_reg-1 downto 0);
+		PCsrc             :  out std_logic;
+		TAddr_MEM_out     :	 out std_logic_vector(instruction_parallelism-1 downto 0)	
 		 );   
 
 end MEM_stage;
--- VHDL/src/RISCV.vhd
+++ VHDL/src/RISCV.vhd
@@ -11,17 +11,15 @@ port(
 	clk					:in std_logic;
 	rst                 :in std_logic;
 	instruction_ID_in   :in std_logic_vector(instruction_parallelism-1 downto 0); --to ID stage
-	
+	Read_data_WB_in     :in std_logic_vector(data_parallelism-1 downto 0); --to WB stage
 	--out
 	PCWrite_ID_out      :out std_logic; --from ID stage
 	IF_ID_Write_ID_out  :out std_logic; --from ID stage
 	ALUout_EX_out       :out std_logic_vector(data_parallelism-1 downto 0);--from EX stage
 	ALU_bypass_EX_out   :out std_logic_vector(data_parallelism-1 downto 0);--from EX stage
 	
-	MemRead_data_mem	:out std_logic;
-	memWrite_data_mem   :out std_logic;
-	Add_data_mem        :out std_logic_vector(data_parallelism-1 downto 0);
-	Writedata_data_mem  :out std_logic_vector(data_parallelism-1 downto 0);
+	MemRead	            :out std_logic;
+	MemWrite            :out std_logic;
 );
 
 
@@ -31,7 +29,7 @@ architecture structural of RISCV is
 	port(
 		--in
 		rst						:in std_logic;
-		PC_EX_in				   :in std_logic_vector(instruction_parallelism-1 downto 0);
+		PC_EX_in				 :in std_logic_vector(instruction_parallelism-1 downto 0);
 		imm_EX_in				:in std_logic_vector(data_parallelism-1 downto 0);
 		Read_data1_EX_in		:in std_logic_vector(data_parallelism-1 downto 0);
 		Read_data2_EX_in		:in std_logic_vector(data_parallelism-1 downto 0);
@@ -62,12 +60,14 @@ architecture structural of RISCV is
 		TAddr_EX_out			:out std_logic_vector(instruction_parallelism-1 downto 0);
 		rd_EX_out				:out std_logic_vector(dest_reg-1 downto 0);
 		JAL_PC_4_EX_out		    :out std_logic_vector(instruction_parallelism-1 downto 0);
+		immediate_EX_out        :out std_logic_vector(data_parallelism-1 downto 0);
 		
 		--out WB
 		WB_EX_out	            :out std_logic_vector(WB_length-1 downto 0);
 		
 		--out M
 		M_EX_out			    :out std_logic_vector(M_length-1 downto 0);
+		MemRead_out             :out std_logic;--from EX to ID
 		
 		--forwarding unit special inputs
 		
@@ -107,19 +107,45 @@ architecture structural of RISCV is
 		IF_ID_Write_ID_out   : out std_logic;
 		WB_ID_out            : out std_logic_vector(WB_length -1 downto 0);
 		M_ID_out             : out std_logic_vector(M_length -1 downto 0);
-		EX_ID_out            : out std_logic_vector(EX_length -1 downto 0)
+		EX_ID_out            : out std_logic_vector(EX_length -1 downto 0);
+		rs1_ID_out           : out std_logic_vector(source_reg - 1 downto 0);
+		rs2_ID_out           : out std_logic_vector(source_reg - 1 downto 0)
 	);    
 	end component;
 	
-	--component reg_instruction_IF_ID  --ho tolto questo registro perchè va inserito fuori dal RISC_V insieme alla instruction memory
-	--port(
-	--	clk        : in std_logic;
-	--	rst        : in std_logic;
-	--	IF_ID_write: in std_logic;
-	--	d          :  in std_logic_vector(instruction_parallelism-1 downto 0);
-	--	q          :  out std_logic_vector(instruction_parallelism-1 downto 0)		
-	--);    
-	--end component;
+	component MEM_stage
+	port(
+		--inputs
+		WB_MEM_in         :  in std_logic_vector(WB_length -1 downto 0);
+		BranchCtrl		  :  in std_logic;
+		JAL_PC_4_MEM_in	  :  in std_logic_vector(instruction_parallelism - 1 downto 0);
+		rd_MEM_in         :  in std_logic_vector(dest_reg-1 downto 0);
+		TAddr_MEM_in      :	 in std_logic_vector(instruction_parallelism-1 downto 0);
+		z_MEM_in          :	 in std_logic;
+		immediate_MEM_in  :  in std_logic_vector(data_parallelism - 1 downto 0);
+		ALUout_MEM_in     :  in std_logic_vector(data_parallelism-1 downto 0);
+		
+		--outputs
+		WB_MEM_out        :	 out std_logic_vector(WB_length -1 downto 0);
+		JAL_PC_4_MEM_out  :  out std_logic_vector(instruction_parallelism - 1 downto 0);
+		ALUout_MEM_out    :  out std_logic_vector(data_parallelism-1 downto 0);
+		immediate_MEM_out :  out std_logic_vector(data_parallelism - 1 downto 0);
+		rd_MEM_out        :  out std_logic_vector(dest_reg-1 downto 0);
+		PCsrc             :  out std_logic;
+		TAddr_MEM_out     :	 out std_logic_vector(instruction_parallelism-1 downto 0)	
+	);
+	end component;
+	
+	component WB_stage
+	port(
+		ALUout_WB_in        :    in std_logic_vector(instruction_parallelism-1 downto 0);
+	    immediate_WB_in     :    in std_logic_vector(instruction_parallelism-1 downto 0);
+	    JAL_PC_4_WB_in      :    in std_logic_vector(instruction_parallelism-1 downto 0);
+	    Read_data_WB_in     :    in std_logic_vector(data_parallelism-1 downto 0);
+	    sel_WB_in           :    in std_logic_vector(2 downto 0);
+	    output_WB_out       :    out std_logic_vector(data_parallelism-1 downto 0)
+	);
+	end component;
 	
 	component reg_jal_PC_IF_ID
 	port(
@@ -144,25 +170,21 @@ architecture structural of RISCV is
 	signal JAL_IF_out_s, PC_IF_out_s : std_logic_vector(instruction_parallelism - 1 downto 0); -- IF stage output signals --IF/ID pipeline regs input
 	signal JAL_ID_in_s, PC_ID_in_s : std_logic_vector(instruction_parallelism - 1 downto 0); -- ID stage input signals --IF/ID pipeline regs output
 	signal IF_ID_Write_ID_out_s: std_logic; --output signal from ID stage to IF/ID pipeline regs to hazard detection
-	signal RegWrite_ID_in_s: std_logic; --from wb stage to ID stage
 	signal ID_EX_MemRead_ID_in_s: std_logic; --from ex stage to ID stage
-	signal write_register_ID_in_s: std_logic_vector(length_in_RF-1 downto 0); --from WB stage to ID stage
-	signal write_data_ID_in_s: std_logic_vector(data_parallelism - 1 downto 0); --from WB stage to ID stage
 	signal rd_backward_ID_in_s: std_logic_vector(source_reg - 1 downto 0); --from EX stage to ID stage
-	signal JAL_ID_out_s, PC_ID_out_s, PC_EX_in_s, JAL_EX_in_s, TAddr_EX_out_s, JAL_PC_4_EX_out_s: std_logic_vector(instruction_parallelism - 1 downto 0);
-	signal read_data_1_ID_out_s, read_data_2_ID_out_s, immediate_ID_out_s, imm_EX_in_s, Read_data1_EX_in_s, Read_data2_EX_in_s: std_logic_vector(data_parallelism-1 downto 0);
+	signal JAL_ID_out_s, PC_ID_out_s, PC_EX_in_s, JAL_EX_in_s, TAddr_EX_out_s, JAL_PC_4_EX_out_s, JAL_PC_4_MEM_in_s, TAddr_MEM_in_s, JAL_PC_4_MEM_out_s, TAddr_MEM_out_s: std_logic_vector(instruction_parallelism - 1 downto 0);
+	signal read_data_1_ID_out_s, read_data_2_ID_out_s, immediate_ID_out_s, imm_EX_in_s, immediate_EX_out_s, Read_data1_EX_in_s, Read_data2_EX_in_s, ALUout_EX_out_s, immediate_MEM_in_s, ALUout_MEM_in_s, immediate_MEM_out_s: std_logic_vector(data_parallelism-1 downto 0);
 	signal to_ALU_control_ID_out_s, to_ALU_ctrl_EX_in_s: std_logic_vector(alu_ctrl - 1 downto 0);
 	signal rd_ID_out_s, rd_EX_in_s: std_logic_vector(rd_length - 1 downto 0); 
 	signal PCWrite_ID_out_s: std_logic; --from ID stage to PC and to instruction memory
-	signal WB_ID_out_s, WB_EX_in_s, WB_EX_out_s: std_logic_vector(WB_length -1 downto 0);--from ID stage to ID/EX pipeline registers
-	signal M_ID_out_s, M_EX_in_s, M_EX_out_s: std_logic_vector(M_length -1 downto 0);--from ID stage to ID/EX pipeline registers
+	signal WB_ID_out_s, WB_EX_in_s, WB_EX_out_s, WB_MEM_in_s, WB_MEM_out_s, WB_WB_in_s: std_logic_vector(WB_length -1 downto 0);--from ID stage to ID/EX pipeline registers
+	signal M_ID_out_s, M_EX_in_s, M_EX_out_s, M_MEM_in_s: std_logic_vector(M_length -1 downto 0);--from ID stage to ID/EX pipeline registers
 	signal EX_ID_out_s, EX_EX_in_s: std_logic_vector(EX_length -1 downto 0);--from ID stage to ID/EX pipeline registers
-	signal ALU_backward_MEM_out_s, muxout_backward_WB_out_s: std_logic_vector(data_parallelism-1 downto 0);--from MEM stage to EX stage
-	signal z_EX_out_s: std_logic;--from EX stage to EX/MEM stage
-	signal rs1_EX_in_s, rs2_EX_in_s: std_logic_vector(source_reg-1 downto 0);
-	signal RegWrite_MEM_in_s, RegWrite_WB_in_s: std_logic;
-	signal rd_MEM_in_s, rd_WB_in_s: std_logic_vector(dest_reg-1 downto 0);
-	signal
+	signal ALU_backward_MEM_out_s, output_WB_out_s: std_logic_vector(data_parallelism-1 downto 0);--from MEM stage to EX stage
+	signal z_EX_out_s, z_MEM_in_s: std_logic;--from EX stage to EX/MEM stage
+	signal rs1_ID_out_s, rs2_ID_out_s, rs1_EX_in_s, rs2_EX_in_s: std_logic_vector(source_reg-1 downto 0);
+	signal rd_MEM_in_s, rd_WB_in_s, rd_MEM_out_s: std_logic_vector(dest_reg-1 downto 0);
+	signal PCsrc_s: std_logic; --from MEM stage to IF stage
 	
 
 		
@@ -194,12 +216,12 @@ architecture structural of RISCV is
 						--inputs
 	                    clk					 => clk,
 	                    rst					 => rst,
-	                    RegWrite_ID_in		 => RegWrite_ID_in_s,
+	                    RegWrite_ID_in		 => WB_WB_in_s(0),
 	                    ID_EX_MemRead_ID_in  => ID_EX_MemRead_ID_in_s,
 	                    jal_ID_in			 => JAL_ID_in_s,
 	                    pc_ID_in			 => PC_ID_in_s
-	                    write_register_ID_in => write_register_ID_in_s,
-	                    write_data_ID_in	 => write_data_ID_in_s,
+	                    write_register_ID_in => rd_WB_in_s,
+	                    write_data_ID_in	 => output_WB_out_s,
 	                    instruction_ID_in    => instruction_ID_in,
 	                    rd_backward_ID_in    => rd_backward_ID_in_s,
 	                    --outputs
@@ -214,7 +236,9 @@ architecture structural of RISCV is
 	                    IF_ID_Write_ID_out   => IF_ID_Write_ID_out_s,
 	                    WB_ID_out            => WB_ID_out_s,
 	                    M_ID_out             => M_ID_out_s,
-	                    EX_ID_out  	         => EX_ID_out_s
+	                    EX_ID_out  	         => EX_ID_out_s,
+						rs1_ID_out			 => rs1_ID_out_s,
+						rs2_ID_out			 => rs2_ID_out_s
 						);
 						
 	--ID/EX pipeline registers
@@ -259,7 +283,7 @@ architecture structural of RISCV is
 	                                            q  	=> to_ALU_ctrl_EX_in_s
 												);	
 	
-	reg_rd_ID_EX: reg generic map(32) port map (
+	reg_rd_ID_EX: reg generic map(5) port map (
 												clk => clk,
 	                                            rst => rst,
 	                                            d   => rd_ID_out_s,
@@ -286,6 +310,24 @@ architecture structural of RISCV is
 	                                            q  	=> EX_EX_in_s
 												);	
 	
+	reg_rs1_ID_EX: reg generic map(5) port map (
+												clk => clk,
+	                                            rst => rst,
+	                                            d   => rs1_ID_out_s,
+	                                            q  	=> rs1_EX_in_s
+												);	
+												
+	reg_rs2_ID_EX: reg generic map(5) port map (
+												clk => clk,
+	                                            rst => rst,
+	                                            d   => rs2_ID_out_s,
+	                                            q  	=> rs2_EX_in_s
+												);
+												
+												
+												
+												
+	
 	--EX stage
 	EX: EX_stage port map(
 						--in                   
@@ -298,7 +340,7 @@ architecture structural of RISCV is
 						funct3_EX_in		   => to_ALU_ctrl_EX_in_s(2 downto 0),
 						rd_EX_in			   => rd_EX_in_s,
 						ALU_backward_MEM_out   => ALU_backward_MEM_out_s,
-						muxout_backward_WB_out => muxout_backward_WB_out_s,
+						muxout_backward_WB_out => output_WB_out_s,
 						JAL_PC_4_EX_in		   => JAL_EX_in_s,
 											  
 						-- in EX              
@@ -314,47 +356,184 @@ architecture structural of RISCV is
 											  
 						--out                 
 											  
-						ALUout_EX_out		   => ALUout_EX_out,
+						ALUout_EX_out		   => ALUout_EX_out_s,
 						ALU_bypass_EX_out	   => ALU_bypass_EX_out,
 						z_EX_out			   => z_EX_out_s,
 						TAddr_EX_out		   => TAddr_EX_out_s,
 						rd_EX_out			   => rd_backward_ID_in_s,
 						JAL_PC_4_EX_out	       => JAL_PC_4_EX_out_s,
-											  
+						immediate_EX_out       => immediate_EX_out_s,
+						
 						--out WB               
 						WB_EX_out	           => WB_EX_out_s,
 											  
 						--out M                
 						M_EX_out	           => M_EX_out_s,
-						
+						MemRead_out            =>ID_EX_MemRead_ID_in_s,
 						
 						--forwarding unit special inputs
 						
 						rs1_EX_in			   => rs1_EX_in_s,
 						rs2_EX_in			   => rs2_EX_in_s,
-						RegWrite_MEM_in	       => RegWrite_MEM_in_s,
-						RegWrite_WB_in		   => RegWrite_WB_in_s,
+						RegWrite_MEM_in	       => WB_MEM_in_s(0),
+						RegWrite_WB_in		   => WB_WB_in_s(0),
 						rd_MEM_in			   => rd_MEM_in_s,
 						rd_WB_in			   => rd_WB_in_s
 						);
+					
+	--EX/MEM pipeline registers
+	reg_WB_EX_MEM: reg generic map(4) port map (
+												clk => clk,
+	                                            rst => rst,
+	                                            d   => WB_EX_out_s,
+	                                            q  	=> WB_MEM_in_s
+												);
+												
+	reg_M_EX_MEM:  reg generic map(3) port map (
+												clk => clk,
+	                                            rst => rst,
+	                                            d   => M_EX_out_s,
+	                                            q  	=> M_MEM_in_s
+												);	
+    
+	reg_JAL_EX_MEM: reg generic map(32) port map (
+												clk => clk,
+	                                            rst => rst,
+	                                            d   => JAL_PC_4_EX_out_s,
+	                                            q  	=> JAL_PC_4_MEM_in_s
+												);
+												
+	reg_rd_EX_MEM: reg generic map(5) port map (
+												clk => clk,
+	                                            rst => rst,
+	                                            d   => rd_backward_ID_in_s,
+	                                            q  	=> rd_MEM_in_s
+												);	
+												
+	reg_TAddr_EX_MEM: reg generic map(32) port map (
+												clk => clk,
+	                                            rst => rst,
+	                                            d   => TAddr_EX_out_s,
+	                                            q  	=> TAddr_MEM_in_s
+												);
+	
+	reg_z_EX_MEM: reg generic map(1) port map (
+												clk => clk,
+	                                            rst => rst,
+	                                            d   => z_EX_out_s,
+	                                            q  	=> z_MEM_in_s
+												);
+												
+	reg_ALUout_EX_MEM: reg generic map(32) port map (
+												clk => clk,
+	                                            rst => rst,
+	                                            d   => ALUout_EX_out_s,
+	                                            q  	=> ALUout_MEM_in_s
+												);
+												
+	reg_immediate_EX_MEM: reg generic map(32) port map (
+												clk => clk,
+	                                            rst => rst,
+	                                            d   => immediate_EX_out_s,
+	                                            q  	=> immediate_MEM_in_s
+												);
+	
+
+	--MEM stage
+	MEM: MEM_stage port map(
+	                        --inputs
+							WB_MEM_in         => WB_MEM_in_s,
+							BranchCtrl		  => M_MEM_in_s(0),
+							JAL_PC_4_MEM_in	  => JAL_PC_4_MEM_in_s,
+							rd_MEM_in         => rd_MEM_in_s,
+							TAddr_MEM_in      => TAddr_MEM_in_s,
+							z_MEM_in          => z_MEM_in_s,
+							immediate_MEM_in  => immediate_MEM_in_s,
+							ALUout_MEM_in     => ALUout_MEM_in_s,
+											 
+							--outputs        
+							WB_MEM_out        => WB_MEM_out_s,
+							JAL_PC_4_MEM_out  => JAL_PC_4_MEM_out_s,
+							ALUout_MEM_out    => ALU_backward_MEM_out_s,
+							immediate_MEM_out => immediate_MEM_out_s,
+							rd_MEM_out        => rd_MEM_out_s,
+							PCsrc             => PCsrc_s,
+							TAddr_MEM_out	  => TAddr_MEM_out_s
+							);
+							
+	--MEM/WB pipeline registers
+	reg_WB_MEM_WB: reg generic map(4) port map (
+												clk => clk,
+	                                            rst => rst,
+	                                            d   => WB_MEM_out_s,
+	                                            q  	=> WB_WB_in_s,
+												);
+	
+	reg_JAL_MEM_WB: reg generic map(32) port map (
+												clk => clk,
+	                                            rst => rst,
+	                                            d   => JAL_PC_4_MEM_out_s,
+	                                            q  	=> JAL_PC_4_WB_in_s
+												);
+												
+	reg_ALUout_MEM_WB: reg generic map(32) port map (
+												clk => clk,
+	                                            rst => rst,
+	                                            d   => ALU_backward_MEM_out_s,
+	                                            q  	=> ALUout_WB_in_s
+												);
+												
+	reg_immediate_MEM_WB: reg generic map(32) port map (
+												clk => clk,
+	                                            rst => rst,
+	                                            d   => immediate_MEM_out_s,
+	                                            q  	=> immediate_WB_in_s
+												);	
+
+	reg_rd_MEM_WB: reg generic map(5) port map (
+												clk => clk,
+	                                            rst => rst,
+	                                            d   => rd_MEM_out_s,
+	                                            q  	=> rd_WB_in_s
+												);												
+	
+	--WB stage
+    WB: WB_stage port map(
+						ALUout_WB_in    => ALUout_WB_in_s,
+	                    immediate_WB_in => immediate_WB_in_s,
+	                    JAL_PC_4_WB_in  => JAL_PC_4_WB_in_s,
+	                    Read_data_WB_in => Read_data_WB_in,
+	                    sel_WB_in       => WB_WB_in_s(3 downto 0),
+	                    output_WB_out   => output_WB_out_s;	
+						);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
     PCWrite_ID_out <= PCWrite_ID_out_s;	
-	IF_ID_Write_ID_out <= IF_ID_Write_ID_out_s;
-						
-						
-						
-						
-						
-						
-						
-						
-						
-						
-						
-						
-						
-	--EX/MEM pipeline registers
-	
-	--MEM stage
-	
-	--
+	IF_ID_Write_ID_out <= IF_ID_Write_ID_out_s;	
+	ALUout_EX_out <= ALUout_EX_out_s;
+    MemWrite <= M_MEM_in_s(2);
+	MemRead <= M_MEM_in_s(1);
